c878e00fd1b2af1e7c4117e29553c4cb2d6a8e43
XionWin <xionwin@hotmail.com>
2022-02-14 21:11:16 +0800

    Update

--- Tron.Hardware/GPIODevice.cs
+++ Tron.Hardware/GPIODevice.cs
@@ -1,11 +1,9 @@
-using System;
+using System.Collections.Generic;
 
 namespace Tron.Hardware
 {
-    public abstract class GPIODevice
+    public abstract class GPIODevice<T>
+    where T :IDictionary<string, IGPIO>
     {
-        protected GPIODevice()
-        {
-        }
     }
 }
