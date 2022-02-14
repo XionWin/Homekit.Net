634b722718e82802d841d704ab0edf4874ce071a
XionWin <xionwin@hotmail.com>
2022-02-14 23:01:34 +0800

    Update

--- Homekit/Program.cs
+++ Homekit/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+if (!Tron.Hardware.Library.Init())
+    throw new Exception("Unable to initialize bcm2835 library");
+var relay = new Tron.Device.Relay.RPiRelayBoard.Module();
+
+
